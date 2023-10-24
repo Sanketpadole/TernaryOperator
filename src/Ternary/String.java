0c3b16bd971aefa39b954176f5a9b90b87fa1550
unknown <sanketpadole@nimapinfotech.com>
2023-10-24 13:30:18 +0530

    dgefk

---
+++ src/Ternary/String.java
@@ -0,0 +1,11 @@
+package Ternary;
+
+public class String {
+	public static void main(String[] args) {
+		java.lang.String str = "hello";
+		java.lang.String str1 = str + "world";
+		System.out.println("string" + str);
+
+	}
+
+}
---
+++ src/Ternary/ternaryoperator.java
@@ -0,0 +1,25 @@
+package Ternary;
+
+public class ternaryoperator {
+
+//	public static void main(String[] args) {
+//		int n = 5;
+//		int result;
+//		result = n % 2 == 0 ? 10 : 20;
+//		System.out.println(result);
+//	}
+
+	public static void main(String[] args) {
+		int n = 5;
+		int result;
+		if (n % 2 == 0) {
+			result = 10;
+			System.out.println(result);
+		} else {
+			result = 20;
+			System.out.println(result);
+		}
+
+	}
+
+}
